55e057713e623afdb52bf0f1cc5c18e885e87de8
Eoghan O'Connell <eoclives@hotmail.com>
2021-08-04 13:00:58 +0200

    add tool functionexample

---
+++ tools/rtdc_tools/plot_rtdc_image/plot_rtdc_image.py
@@ -0,0 +1,34 @@
+"""
+Function for plotting an rtdc image
+
+This is just an example function. It isn't very useful.
+"""
+
+
+# import modules/packages at the top of the script
+# remember to make a requirements.txt file with the package versions
+import dclab
+import matplotlib.pyplot as plt
+
+# write your tool (function, class)
+# remember to run flake8 on your script before uploading it
+
+
+def plot_rtdc_image(rtdc_ds, image_n):
+    """Plot the nth image in an rtdc dataset
+
+    Parameters
+    ----------
+    rtdc_ds : rtdc dataset
+    image_n : int
+        The index of the image you wish to plot
+
+    """
+    fig, ax = plt.subplots(1, 1, figsize=(9, 5))
+    ax.imshow(rtdc_ds["image"][image_n])
+    plt.show()
+
+
+# example use of the above function
+ds = dclab.new_dataset("fb719fb2-bd9f-817a-7d70-f4002af916f0")
+plot_rtdc_image(rtdc_ds=ds, image_n=5)
