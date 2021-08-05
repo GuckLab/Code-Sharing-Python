99e70acaa2dd830a34fa1e3424191f18e53a60bb
Eoghan O'Connell <eoclives@hotmail.com>
2021-08-05 14:42:47 +0200

    move -maxdepth to see what macOS says

--- tests/run_all_scripts.sh
+++ tests/run_all_scripts.sh
@@ -5,13 +5,13 @@ for f in $(find -maxdepth 4 -name *.py); do
 
   # create a blank line for clearer debugging
   echo ""
-  echo $f
+  echo "File Location: $f"
   # create a requirements variable
   parentdir="$(dirname "$f")"
-  echo $parentdir
+  echo "Parent Directory: $parentdir"
   # create a requirements variable
   req=$parentdir"/requirements.txt"
-  echo $req
+  echo "Requirements File: $req"
   # create venv
   python -m venv $parentdir/.venv
   # activate venv
